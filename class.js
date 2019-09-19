c2dd19a8c79b61fb9c78cf39ed266fa559f434f1
Lucas Fonte <53484547+Lucas-Fonte@users.noreply.github.com>
2019-09-19 18:41:01 -0300

    class refactor

--- class.js
+++ class.js
@@ -1,23 +1,35 @@
 const productType = ['AGENDA', 'CADERNO', 'SACOLA', 'EMBALAGEM'];
-const productOperations = new Array();
 
-productOperations['AGENDA'] = {
-                                ext: 200, 
-                                gui: 1000, 
-                                silk: 1500, 
-                                corte: 3000
-                            };
+const defaultStructure = ['CAPA', 'CONTRACAPA', 'MIOLO', 'PDO', 'ESPIRAL'];
+
+const productOperations = {ext: 200, gui:1000, silk:1500, corte:3000};
+
+                            
+class Component {
+    constructor(type){
+        this.type = type;
+        this.qtde = 10000;
+        this.operations = productOperations;
+        this.order = Object.keys(productOperations);
+    }
+
+    duration(){
+        return this.order.map(element => (this.qtde/productOperations[element]).toFixed(2));
+    }
+}
 
 class Project {
     constructor([type, qtde]){
             this.type = type;
             this.qtde = qtde;
-            this.setores = productOperations[type];
             this.date = new Date();
+            this.structure = defaultStructure.map(element =>  new Component(element));
+            this.duration = defaultStructure.map(element =>  new Component(element).duration());
+
     };
-    
+
     forecast(){
-        const days = (this.qtde/this.productionHour);
+        const days = (this.qtde/ silkError(this.structure.capa.silk));
         const end = new Date();
         end.setDate((this.date.getDate() + days));
         return end;
@@ -30,4 +42,7 @@ class Project {
 
 const result = ['AGENDA', 10000];
 const produto = new Project(result);
-console.log(produto);
+
+// const component = new Component('CAPA');
+
+console.log(produto.structure, produto.duration);
